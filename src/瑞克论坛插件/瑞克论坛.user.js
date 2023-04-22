c77733df9f11bfef7a72acd240647a7c891290c5
limaobo <web_lmb@foxmail.com>
2023-04-23 01:19:29 +0800

    提交瑞克插件

--- src/瑞克论坛插件/瑞克论坛.user.js
+++ src/瑞克论坛插件/瑞克论坛.user.js
@@ -1,17 +1,58 @@
 // ==UserScript==
 // @name         瑞克论坛插件
-// @namespace    http://tampermonkey.net/
+// @namespace    https://www.ruike1.com/
 // @version      0.1
-// @description  try to take over the world!
+// @description  瑞克网站的一些脚本
 // @author       You
 // @match        *ruike1.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @grant        GM_setClipboard
+// @grant        unsafeWindow
 // ==/UserScript==
 
 (function () {
   ("use strict");
-  // <img id="fx_checkin_b" src="source/plugin/k_misign/static/mini.gif" alt="点击签到" style="position:relative;top:5px;height:18px;">
-  let;
-  // Your code here...
+
+  let signInBtn = document.querySelector("#fx_checkin_b");
+  let thread_types = document.querySelector("#thread_types");
+  // 自动签到
+  if (signInBtn.alt === "点击签到") {
+    signInBtn.click();
+  }
+  if (thread_types) {
+    thread_types.insertAdjacentHTML(
+      "beforeend",
+      `<li class='copy-wrap'><a style="background: #1890ff;color: #fff;cursor: pointer;">复制标题</a></li>`
+    );
+    let copyBtn = document.querySelector(".copy-wrap");
+    let row = document.querySelectorAll('[id^="normalthread"] > tr');
+    row.forEach((item) => {
+      item.insertAdjacentHTML(
+        "afterbegin",
+        `<td class="checkbox-td" style="width:30px;text-align:center"><input type="checkbox" name="select-title" class="checkbox-select" style="width:20px;height:20px"></td>`
+      );
+    });
+
+    copyBtn.onclick = function (event) {
+      let checkbox = document.querySelectorAll(".checkbox-select");
+      let titleAry = [];
+      let noCheckAry = [];
+      checkbox.forEach((item) => {
+        let parent =
+          item.parentElement.tagName === "TR"
+            ? item.parentElement
+            : item.parentElement.parentElement;
+        let text = parent.querySelector(".common .xst").innerText;
+
+        if (item.checked) {
+          titleAry.push(text);
+        } else {
+          noCheckAry.push(text);
+        }
+      });
+      let checkStr = titleAry.join("\n");
+      let noCheckStr = noCheckAry.join("\n");
+      GM_setClipboard(checkStr ? checkStr : noCheckStr);
+    };
+  }
 })();
