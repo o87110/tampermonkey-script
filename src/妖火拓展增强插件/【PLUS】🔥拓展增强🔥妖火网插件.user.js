a19f2bdd7c5c7040f8b47caf58493a90af2ccf5b
limaobo <56180145@qq.com>
2023-05-05 14:57:08 +0800

    修改随机数

--- src/妖火拓展增强插件/【PLUS】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【PLUS】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【PLUS自用】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      2.3.8
+// @version      2.3.9
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -1268,7 +1268,7 @@
         let eatImgSrc = "/NetImages/li.gif";
 
         let eatList = document.querySelectorAll(`img[src='${eatImgSrc}']`);
-        let randomNum = getRandomNumber(8, 25);
+        let randomNum = getRandomNumber(8, 20);
 
         for (let index = 0; index < eatList.length; index++) {
           const element = eatList[index];
