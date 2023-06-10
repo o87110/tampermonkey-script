fbc2ad98dfbb79e048426ba78da8d058e1934b0c
limaobo <web_lmb@foxmail.com>
2023-06-10 11:57:02 +0800

    允许小数点

--- src/妖火拓展增强插件/【PLUS】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【PLUS】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【PLUS自用】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      3.5.1
+// @version      3.5.2
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -2113,7 +2113,7 @@
                 // /^\s*\d+(?:,\s*\d+)*,?\s*$/
                 if (
                   !value ||
-                  /^\s*\d+(?:\.\d+)?(?:,\s*\d+(?:\.\d+)?)*,?\s*$/.test(value)
+                  /^\s*\d+(?:\.\d+)?(?:,\s*\d+(?:\.\d+)?)*,?\.?\s*$/.test(value)
                 ) {
                   item.value = value;
                   previousValue = value;
