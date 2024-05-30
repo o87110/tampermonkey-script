f2e7ffc861d6f6565bf3e4092f8240ecef2aa6c3
limaobo <56180145@qq.com>
2024-05-30 18:02:26 +0800

    优化吃牛

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      4.12.0
+// @version      4.12.1
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -4760,7 +4760,8 @@
         if (!timer) {
           addInterval(list.length);
         }
-        for (const item of list) {
+        let newList = Array.from(list).reverse()
+        for (const item of newList) {
           let match = item.innerHTML.match(/\((\d+)妖晶\)$/);
           let number = parseInt(match[1]);
           let href = item.getAttribute("href");
@@ -4777,6 +4778,9 @@
                 `当前大于设置的赌注妖精：${eatBoastMaxNum}，则不自动吃`
               );
             }
+          } else {
+            console.log('妖精小于设置金额，已关闭自动吃牛');
+            clearInterval(timer)
           }
         }
       }
