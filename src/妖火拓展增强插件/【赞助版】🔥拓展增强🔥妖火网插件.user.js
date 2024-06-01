1ddd5632036a578385c7fb641098d1b6222daee9
limaobo <web_lmb@foxmail.com>
2024-06-01 21:39:01 +0800

    更新

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      4.14.0
+// @version      4.14.1
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -4630,7 +4630,7 @@
       list = Array.from(list).filter((item) => {
         let match = item.innerHTML.match(/\((\d+)妖晶\)$/);
         let number = parseInt(match[1]);
-        if (number >= parseFloat(filterBoastMoney)) {
+        if (number > parseFloat(filterBoastMoney)) {
           item.parentNode.remove();
           return false;
         } else {
