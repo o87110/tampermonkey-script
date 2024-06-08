41a3a545621f691db78a16bd2844806cabf42d40
limaobo <web_lmb@foxmail.com>
2024-06-08 14:42:15 +0800

    修改bug

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      4.15.1
+// @version      4.16.0
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -4548,8 +4548,8 @@
       MY_setValue("boastConfig", boastConfig);
     }
     if (consecutiveCount >= randomConsecutive) {
-      randomNumber = previousNumber === 1 ? 2 : 1; // 切换到另一个数字
-      console.log(`大于连续次数${randomConsecutive}，答案反转`)
+      randomNumber = parseInt(previousNumber) === 1 ? 2 : 1; // 切换到另一个数字
+      console.log(`大于连续次数${randomConsecutive}，答案反转${randomNumber}`);
     }
     return randomNumber;
   }
@@ -4562,7 +4562,7 @@
       previousAry = [],
     } = boastConfig;
 
-    if (randomNumber === previousNumber) {
+    if (parseInt(randomNumber) === parseInt(previousNumber)) {
       consecutiveCount++;
     } else {
       if (consecutiveCount >= randomConsecutive) {
@@ -4793,7 +4793,7 @@
         }
         // autoPublishBoastInterval
         console.log("nextBoastData", nextBoastData);
-        
+
         // 打印动态概率
         if (isPublishBoastDynamicWinRate) {
           $(".boast-index-rate").text(
@@ -5083,8 +5083,6 @@
             },
             true
           );
-
-          
 
           select.insertAdjacentHTML(
             "afterend",
