91713921db39a7cdcc730692c6f6e663187c6213
limaobo <56180145@qq.com>
2024-11-26 10:21:37 +0800

    优化间隔

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      5.9.1
+// @version      5.9.2
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -5968,10 +5968,10 @@ void (async function () {
     }
     function addInterval(isFinished) {
       autoPublishBoastInterval = isFinished
-        ? parseInt(autoPublishBoastInterval) - 25
+        ? parseInt(autoPublishBoastInterval) - 50
         : parseInt(autoPublishBoastInterval) + 5;
       if (autoPublishBoastInterval <= 5) {
-        autoPublishBoastInterval = 5;
+        autoPublishBoastInterval = 1;
       }
       if (autoPublishBoastInterval >= 50) {
         autoPublishBoastInterval = 50;
