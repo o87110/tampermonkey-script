7c3b4f28e614d91d4dac062dc9b64ca57ab9f869
limaobo <56180145@qq.com>
2025-01-07 00:03:32 +0800

    测试自动更新

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      6.0.0
+// @version      6.0.1
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -3471,7 +3471,6 @@ void (async function () {
 
     if (status === "edit") {
       $("#replyTextarea").on("change", function (event) {
-        // console.warn(event.target.value);
         let value = event.target.value;
         this.value = value
           .split("\n")
