b2081dd3ebe5ab06fed6e2943d981480d270b17c
limaobo <56180145@qq.com>
2025-01-13 16:14:54 +0800

    更新

--- src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
+++ src/妖火拓展增强插件/【赞助版】🔥拓展增强🔥妖火网插件.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【赞助版】🔥拓展增强🔥妖火网插件
 // @namespace    https://yaohuo.me/
-// @version      6.1.0
+// @version      6.1.1
 // @description  发帖ubb增强、回帖ubb增强、查看贴子显示用户等级增强、半自动吃肉增强、全自动吃肉增强、自动加载更多帖子、自动加载更多回复、支持个性化菜单配置
 // @author       龙少c(id:20469)开发，参考其他大佬：外卖不用券(id:23825)、侯莫晨、Swilder-M
 // @match        *://yaohuo.me/*
@@ -1099,7 +1099,7 @@ void (async function () {
     let type = "text";
     let isNewReply = false;
     if (isPage && isOpenFilterPostsReply) {
-      if (!result.id.length && !result.name.length && !result.text.length) {
+      if (!Object.values(result).flat().length) {
         console.info("当前没有设置过滤项无需过滤");
         return false;
       }
